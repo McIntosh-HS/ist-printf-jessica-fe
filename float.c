88c8477de2716d1de83784c54543728076002d6c
jessica <2027felker.jessica@fcboe.com>
2024-01-18 14:49:42 -0500

    float

--- float.c
+++ float.c
@@ -5,6 +5,6 @@ int main() {
   float tax = .09;
   printf("Your subtotal is: %.2f\n", subtotal);
   printf("The tax rate is: %.2f\n", tax);
-  printf("The final total is: %f\n", subtotal + subtotal*tax);
+  printf("The final total is: %.2f\n", subtotal + subtotal*tax);
   return 0;
 }
