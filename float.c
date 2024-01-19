25f72aae39a70f6e733cd41fd03f1e53340c5a1e
jessica <2027felker.jessica@fcboe.com>
2024-01-19 14:21:25 -0500

    final total

--- float.c
+++ float.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main() {
-  float subtotal = 23.84;
-  float tax = .09;
-  printf("Your subtotal is: %.2f\n", subtotal);
-  printf("The tax rate is: %.2f\n", tax);
+  float subtotal;
+  float tax;
+  printf("How much are your groceries? ", subtotal);
+  scanf("%f", &subtotal);
+  printf("How much is your tax rate? ", tax); // ask the user for the tax rate
+  scanf("%f", &tax); // put user input in tax variable
   printf("The final total is: %.2f\n", subtotal + subtotal*tax);
   return 0;
 }
